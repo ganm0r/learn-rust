f68b88458f3c0655c14790b23e32be4caf590458
ganm0r <gndhrvmv@gmail.com>
2023-05-13 13:03:06 +0530

    👔 add & compare random number with user guess

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 use std::io::Write;
 
 fn main() {
     println!("\nguess the number!");
-    
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    print!("your guess: ");
+    println!("the secret number is: {secret_number}");
 
-    io::stdout().flush().expect("failed to flush");
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        print!("\nyour guess: ");
 
-    print!("you guessed: {guess}");
+        io::stdout().flush().expect("failed to flush");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                print!("please enter a number!\n");
+                continue;
+            },
+        };
+
+        print!("you guessed: {guess}\nverdict: ");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => print!("too small!\n"),
+            Ordering::Greater => print!("too big!\n"),
+            Ordering::Equal => {
+                print!("you win!\n");
+                break;
+            },
+        }
+    }
 }
